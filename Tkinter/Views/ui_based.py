b667470c78ad7542eb78a6f344d6b7f2db5bd62e
Vinay Pant <vinaypant24@gmail.com>
2023-05-27 00:07:43 +0530

    initial files

---
+++ Tkinter/Views/ui_based.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+from typing import Any 
+
+
+
+
+
+class styles():
+
+    def __init__(self) -> None:
+        pass
+
+
+
+class ui_elements(styles):
+
+    def __init__(self) -> None:
+        super().__init__()
+
